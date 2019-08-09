9c598393cca84946a429287be2bfc4ed489a1dfb
Daniel Micay <danielmicay@gmail.com>
2019-08-08 21:49:09 -0400

    SdkSetup: add permission whitelist

--- apps/SdkSetup/Android.mk
+++ apps/SdkSetup/Android.mk
@@ -1,4 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := permissions_com.android.sdksetup.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
@@ -10,5 +19,7 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 
 LOCAL_PROGUARD_ENABLED := disabled
+
+LOCAL_REQUIRED_MODULES := permissions_com.android.sdksetup.xml
 
 include $(BUILD_PACKAGE)
